609e6abb22197dc89c6657d9a2e0187618a277d3
susanboratynska <susan.boratynska@gmail.com>
2020-04-13 00:48:18 -0400

    Commit before Yelp composer

--- index.php
+++ index.php
@@ -1,31 +1,133 @@
+<?php
+session_start();
+
+require_once 'Yelp.php';
+
+$url = 'https://api.yelp.com/v3/businesses/search';
+$api_key = 'yWHPoBGMrZDEzX8sVYy5O14ujPsRlJfrADx3p8QbTLlxGWWudCVggIlHOuRmW9AFlJNRG-hvDCFBLs8uTEXlxpCT6qBLCBdqnaJYMUiWQwwED7S9mDmWZFWElFeSXnYx';
+
+$destinationsearch = "";
+$destinationsearch = json_encode($_GET['pac-input']);
+
+
+$parameters = array (
+    'term' => 'restaurants',
+    'location' => json_encode($_GET['pac-input']),
+//    'radius' => 10000,
+    'categories' => 'restaurants',
+    'sort_by' => 'best_match',
+    'limit' => '5'
+//    'price' => '1,2,3',
+//    'attributes' => 'hot_and_new'
+);
+
+print_r(json_encode($_GET['pac-input']));
+
+$options = array ('http' =>
+    array (
+        'header' =>  'Authorization: Bearer ' . $api_key,
+        'method' => 'GET'
+        //'content' => json_encode($data)
+    )
+);
+
+$str = stream_context_create($options);
+$result = json_decode(file_get_contents($url, false, $str));
+print_r($result);
+
+$y = new Yelp();
+$yelp = $y->yelpSearchResults($destinationsearch);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <title>HTTP5203B - Assignment 3 - Susan Boratynska</title>
-        <link rel="stylesheet" type="text/css" href="styles/styles.css" />
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="scripts/scripts.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="styles/styles.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
     <h1>Travel Destination Trends</h1>
 
-    <input id="search__maps" class="controls" type="text" size="55" placeholder="Where do you want to go?">
+    <div class="form__destination">
 
-    <div id="container__googlemaps">
+        <form method="Get" action="index.php">
+            <input id="pac-input"  name="pac-input" type="text" placeholder="Where do you want to go?">
+            <input type="submit" name="submit__destination" id="submit__destination" value="Take Me There"/>
+        </form>
+
+
+
+<!--            <input id="search__maps" class="controls" type="text"  size="55"  placeholder="Where do you want to go?">-->
+
+
 
 
     </div>
+    <div class="row">
 
-    <!-- Included libraries=places for autocomplete search; Enabled places  -->
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA51SWNimZff6jD0kPbF95HRXocqTE1k4&callback=initializeMapJQuery&libraries=places&callback=initAutocomplete">
-    </script>
+        <div class="col-12 col-md-8">
 
-    <div id="container__pinterest"></div>
+<!--            <input id="search__maps" class="controls" type="text" size="55" placeholder="Where do you want to go?">-->
 
-    <div id="container__yelp"></div>
+            <div id="container__googlemaps">
+
+            </div>
+        </div>
+        <!-- Included libraries=places for autocomplete search; Enabled places  -->
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA51SWNimZff6jD0kPbF95HRXocqTE1k4&callback=initializeMapJQuery&libraries=places&callback=initAutocomplete">
+        </script>
 
 
+        <div id="container__pinterest"></div>
+
+        <div id="container__yelp" class="col-6 col-md-4">
+            <div class="yelp__result">
+                <h4 class="yelp__title">Restaurant Name</h4>
+                <div class="row">
+                    <img class="col-4 yelp__images" src="images/pai.jpg">
+                    <div class="col-8">
+                        <div class="yelp__rating">Ratings</div>
+                        <div class="yelp__price">Price</div>
+                        <div class="yelp__description">Description</div>
+                    </div>
+                </div>
+            </div>
+            <div class="yelp__result">
+                <h4 class="yelp__title">Restaurant Name</h4>
+                <div class="row">
+                    <img class="col-4 yelp__images" src="images/pai.jpg">
+                    <div class="col-8">
+                        <div class="yelp__rating">Ratings</div>
+                        <div class="yelp__price">Price</div>
+                        <div class="yelp__description">Description</div>
+                    </div>
+                </div>
+            </div>
+            <div class="yelp__result">
+                <h4 class="yelp__title">Restaurant Name</h4>
+                <div class="row">
+                    <img class="col-4 yelp__images" src="images/pai.jpg">
+                    <div class="col-8">
+                        <div class="yelp__rating">Ratings</div>
+                        <div class="yelp__price">Price</div>
+                        <div class="yelp__description">Description</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     </body>
 </html>
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,71 +2,101 @@
 // SRC: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
 
 function initAutocomplete(listener) {
-    var map = new google.maps.Map(document.getElementById('container__googlemaps'), {
-        center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13,
-        mapTypeId: 'roadmap'
-    });
+    // var map = new google.maps.Map(document.getElementById('container__googlemaps'), {
+    //     center: {lat: -33.8688, lng: 151.2195},
+    //     zoom: 13,
+    //     mapTypeId: 'roadmap'
+    // });
 
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('search__maps');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
-    });
+
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+
+
+
+
+    var toronto = new google.maps.LatLng(43.640, -79.394);
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var map = new google.maps.Map(
+        document.getElementById('container__googlemaps'), {center: toronto, zoom: 15});
+
+    // console.log(findGetParameter('pac-input'));
+    var request = {
+        query: findGetParameter('pac-input'),
+        fields: ['name', 'geometry'],
+    };
+
+    var service = new google.maps.places.PlacesService(map);
 
     var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-        var places = searchBox.getPlaces();
 
-        if (places.length == 0) {
-            return;
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+
+    service.findPlaceFromQuery(request, function(results, status) {
+        console.log(results);
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            // for (var i = 0; i < results.length; i++) {
+            //     createMarker(results[i]);
+            // }
+            results.forEach(function(place) {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                   // return;
+                }
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                    map: map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+
+            map.setCenter(results[0].geometry.location);
         }
-
-        // Clear out the old markers.
-        markers.forEach(function(marker) {
-            marker.setMap(null);
-        });
-        markers = [];
-
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
     });
+
+
 }
+
+
+// PURPOSE: Get variables
+// SRC: https://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+
 
 
 

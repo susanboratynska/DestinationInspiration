e71af8c22ddda1c6d20d2f0589c035a542207589
susanboratynska <susan.boratynska@gmail.com>
2020-04-16 21:47:34 -0400

    CSS and design

--- index.php
+++ index.php
@@ -21,13 +21,16 @@ $url = 'https://api.yelp.com/v3/businesses/search';
 $api_key = 'yWHPoBGMrZDEzX8sVYy5O14ujPsRlJfrADx3p8QbTLlxGWWudCVggIlHOuRmW9AFlJNRG-hvDCFBLs8uTEXlxpCT6qBLCBdqnaJYMUiWQwwED7S9mDmWZFWElFeSXnYx';
 
 $destinationsearch = "";
-$destinationsearch = json_encode($_GET['pac-input']);
-// print_r($destinationsearch);
+
+if (isset($_GET['pac-input'])){
+    $destinationsearch = json_encode($_GET['pac-input']);
+}
+
 
 
 $parameters = array (
     'term' => 'restaurants',
-    'location' => json_encode($_GET['pac-input']),
+    'location' => $destinationsearch,
     'radius' => 10000,
     'categories' => 'restaurants',
     'sort_by' => 'best_match',
@@ -59,58 +62,83 @@ $results = $client->getBusinessesSearchResults($parameters);
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
-    <h1>Travel Destination Trends</h1>
 
-
-    <div class="form__destination">
-        <form method="GET" action="index.php">
-            <input id="pac-input"  name="pac-input" type="text" placeholder="Where do you want to go?">
-            <input id="submit__destination" name="submit__destination" type="submit" value="Take Me There"/>
-        </form>
-    </div>
-
-    <div class="row">
-
-        <div class="col-12 col-md-8" id="container__google">
-            <div id="container__googlemaps"></div>
-        </div>
-        <!-- Included libraries=places for autocomplete search; Enabled places  -->
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA51SWNimZff6jD0kPbF95HRXocqTE1k4&callback=initializeMapJQuery&libraries=places&callback=initAutocomplete">
-        </script>
-
-        <div id="container__yelp" class="col-6 col-md-4">
-        <?php
-        foreach ($results->businesses as $result){
-        ?>
-            <div class="yelp__result">
-                <div class="row">
-                    <div class="yelp__image_container">
-                        <img class="yelp__images" src="<?= $result->image_url; ?>">
-                    </div>
-                    <div class="col-8 yelp__contentbox">
-                        <div class="yelp__title"><a href="<?= $result->url ; ?>" target="_blank"><?= $result->name ; ?></a></div>
-                        <div class="yelp__rating font_12">Rating: <?= $result->rating ; ?></div>
-                        <div class="yelp__price font_12"><?= $result->price ; ?></div>
-                        <div class="yelp__address1 font_12"><?= $result->location->display_address[0] ; ?></div>
-                        <div class="yelp__address2 font_12"><?= $result->location->display_address[1] ; ?></div>
-                    </div>
+        <header>
+            <div class="page-wrapper">
+                <div id="header__div">
+                    <h1 id="header__nav">Destination Inspiration</h1>
                 </div>
             </div>
-        <?php
-        }
-        ?>
+        </header>
+
+        <div id="banner"></div>
+
+        <div id="banner__text">
+            <div class="page-wrapper">
+                <p>
+                    As we're confined to our homes amidst a global pandemic, we're left to explore the world through the internet.
+                    For some of us, this mean travel plans are put on hold but we can still explore the world online.
+                </p>
+                <p>
+                    Enter a destination that you want to learn more about. We'll show you on a map where that is, recommend some restaurants, and show you some pictures of nearby tourist attractions.
+                </p>
+            </div>
+        </div>
+
+
+
+    <div class="page-wrapper">
+        <div class="form__destination">
+            <form method="GET" action="index.php">
+                <input id="pac-input"  name="pac-input" type="text" placeholder="Where do you want to go?">
+                <input id="autocompletevalue_placeid"  name="location_placeid" type="hidden" >
+                <input id="submit__destination" name="submit__destination" type="submit" value="Take Me There"/>
+            </form>
+        </div>
+
+        <div class="row" id="container__map_yelp">
+
+            <div class="col-12 col-md-8" id="container__google">
+                <h2>Map</h2>
+                <div id="container__googlemaps"></div>
+            </div>
+            <!-- Included libraries=places for autocomplete search; Enabled places  -->
+            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmWqyRsPhX5MvyF8R1vG_RV-CuAVNRg98&libraries=places&callback=initAutocomplete">
+            </script>
+
+            <div id="container__yelp" class="col-6 col-md-4">
+                <h2>Restaurants</h2>
+            <?php
+            // print_r($results);
+            for ($i = 0; $i <4; $i++ ){
+                
+            }
+            foreach ($results->businesses as $result){
+            ?>
+                <div class="yelp__result">
+                    <div class="row">
+                        <div class="yelp__image_container">
+                            <img class="yelp__images" src="<?= $result->image_url; ?>">
+                        </div>
+                        <div class="col-8 yelp__contentbox">
+                            <div class="yelp__title"><a href="<?= $result->url ; ?>" target="_blank"><?= $result->name ; ?></a></div>
+                            <div class="yelp__rating font_11">Rating: <?= $result->rating ; ?></div>
+                            <div class="yelp__price font_11"><?= $result->price ; ?></div>
+                            <div class="yelp__address1 font_11"><?= $result->location->display_address[0] ; ?></div>
+                            <div class="yelp__address2 font_11"><?= $result->location->display_address[1] ; ?></div>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+            </div>
+        </div>
+        <div class="row">
+            <h2>Nearby Attractions</h2>
+            <div id="google__places_photo"></div>
         </div>
     </div>
-    <div class="row">
-        <div id="google__places_photo"></div>
-        <img id="google__results_image" >
-<!--        <script async defer src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=--><?//=$api_key?><!--"></script>-->
-        <script async defer src="https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=photo,name,rating,formatted_phone_number&key=<?=$api_key?>"></script>
-    </div>
-    <div id="googleplace">
-        https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=yWHPoBGMrZDEzX8sVYy5O14ujPsRlJfrADx3p8QbTLlxGWWudCVggIlHOuRmW9AFlJNRG-hvDCFBLs8uTEXlxpCT6qBLCBdqnaJYMUiWQwwED7S9mDmWZFWElFeSXnYx
-    </div>
-
     </body>
 </html>
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,6 +23,83 @@ function initAutocomplete(listener) {
     };
 
 
+
+
+
+// Obtain placeid value based on autocomplete event listener and store it in hidden fields of the form:
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        document.getElementById('autocompletevalue_placeid').value = place.place_id;
+        // document.getElementById('autocompletevalue_lat').value = place.geometry.location.lat();
+        // document.getElementById('autocompletevalue_long').value = place.geometry.location.lng();
+        console.log(place.place_id);
+    });
+
+
+// Retrieve lat/long values from get parameter:
+    // var locationlatlong = new google.maps.LatLng(findGetParameter('location__lat'), findGetParameter('location__long') );
+
+// Retrieve place_id from parameter:
+    var autocomplete_placeid = findGetParameter('location_placeid');
+    console.log(autocomplete_placeid);
+
+// Put the placeiD value in the request object:
+    var photorequest = {
+        placeId: autocomplete_placeid,
+        // radius: '1000',
+       fields: ['name', 'photos']
+    };
+
+// Create a new instance of PlacesService:
+    var service2 = new google.maps.places.PlacesService(map);
+
+
+// Find nearby results:
+    service2.getDetails(photorequest, callback);
+
+
+var google__places_photo = document.getElementById('google__places_photo');
+// Iterate through the results:
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+
+            console.log(results.photos);
+
+            // Places details returns 10 photos max:
+            for (var i = 0; i <= results.photos.length ; i++) {
+
+                console.log(results.photos[i].getUrl());
+
+                google__places_photo.innerHTML += '<img class="google__photos" src=' + results.photos[i].getUrl() +'>';
+
+            }
+        }
+    }
+
+
+
+
+// // Get photos from place.geometry:
+// var placesarray = place.photos;
+// // console.log(placesarray[0].getUrl());
+//
+// console.log(placesarray.length);
+//
+// for(var i = 0 ; i <= placesarray.length; i++){
+//     console.log(placesarray[i].getUrl());
+//     document.getElementById('google__results_image').setAttribute('src', placesarray[i].getUrl());
+// }
+//
+//
+
+
+
+
+
+
+
+
     var service = new google.maps.places.PlacesService(map);
 
     // Emptry array for markers:
@@ -62,17 +139,19 @@ function initAutocomplete(listener) {
                     bounds.extend(place.geometry.location);
                 }
 
-                console.log(place);
-                // Get photos from place.geometry:
-                var photoplacesarray = place.photos;
-                // console.log(photoplacesarray[0].getUrl());
-
-                console.log(photoplacesarray.length);
-
-                for(var i = 0 ; i <= photoplacesarray.length; i++){
-                    console.log(photoplacesarray[i].getUrl());
-                    document.getElementById('google__results_image').setAttribute('src', photoplacesarray[i].getUrl());
-                }
+                //
+                // console.log(place);
+                //
+                // // Get photos from place.geometry:
+                // var photoplacesarray = place.photos;
+                // // console.log(photoplacesarray[0].getUrl());
+                //
+                // console.log(photoplacesarray.length);
+                //
+                // for(var i = 0 ; i <= photoplacesarray.length; i++){
+                //     console.log(photoplacesarray[i].getUrl());
+                //     document.getElementById('google__results_image').setAttribute('src', photoplacesarray[i].getUrl());
+                // }
 
             });
 
@@ -81,7 +160,15 @@ function initAutocomplete(listener) {
     });
 
 
+
 }
+
+
+
+
+
+
+
 
 
 // PURPOSE: Get variables
